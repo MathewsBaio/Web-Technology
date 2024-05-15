453010e5270fa4fad89fac5ac07d8db69833437b
Mathews <baiomathews@gmail.com>
2024-05-15 20:08:22 -0300

    pokedex

---
+++ exercícioPoke/script.js
@@ -0,0 +1,54 @@
+// Usar pokeapi para mostrar o primeiro pokemon da pokedex
+// Exibir o número, nome, sprite e uma lista com os tipos do pokemon
+// Exibir um botão de próximo para avançar 1 número
+// Exibir um botão de anterior, caso num for > 0, recuar 1 número
+
+// sprites front_default
+// types (array) (listar nomes)
+
+// https://pokeapi.co/api/v2/pokemon/${textPokemon.value}`
+
+
+let nome = document.querySelector('#name');
+let numPara = document.querySelector('#num');
+let list = document.querySelector('ul');
+let image = document.querySelector('img');
+let proxButton = document.querySelector('#prox')
+let antButton = document.querySelector('#ant');
+let num = 1;
+let tipos;
+
+proxButton.addEventListener('click', e => {
+    num++;
+    numPara.textContent = num; 
+    requestPokemon(num);
+})
+
+antButton.addEventListener('click', e => {
+    if(num > 1) {
+        num--;
+        numPara.textContent = num;
+        requestPokemon(num);
+    }
+})
+
+
+async function requestPokemon(value) {
+    const dataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${value}`)
+    const dataJson = await dataResponse.json();
+    
+        nome.textContent = `Nome: ${dataJson.name}`
+
+        list.innerHTML = ' '
+        dataJson.types.forEach(type => {
+            
+            list.innerHTML += `<li>Tipo: ${type.type.name}</li>` 
+        });
+
+        image.src = `${dataJson.sprites.front_default}`
+        
+    
+}
+
+
+
--- topico11_json/poke.js
+++ topico11_json/poke.js
@@ -6,19 +6,33 @@ let para = document.querySelector("#poke");
 form.addEventListener('submit', p => {
     p.preventDefault();
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${textPokemon.value}`).then(response => {
-        if (response === 404) {
-            para.innerText = "Pokemon não encontrado!";
-        } else {
-            return response.json();
-        }
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${textPokemon.value}`).then(response => {
+    //     if (response === 404) {
+    //         para.innerText = "Pokemon não encontrado!";
+    //     } else {
+    //         return response.json();
+    //     }
         
-    }).then(data => {
-        para.innerHTML = `<img src="${data.sprites.front_default}>"`
-    })
+    // }).then(data => {
+    //     para.innerHTML = `<img src="${data.sprites.front_default}>"`
+    // })
 
     //async e await
 
+
+    async function startSearch() {
+        const dataResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${textPokemon.value}`);
+        const dataJSON = await dataResponse.json();
+        if(dataResponse.status === 404) {
+            para.innerHTML = `Pokemon não encontrado!`    
+        } else {
+            para.innerHTML = `${dataJSON.name}`
+        }
+        
+
+    }
+
+    startSearch();
 
 } )
 
