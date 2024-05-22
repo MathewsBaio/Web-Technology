b405b683408d8f8bf0bebece9beaece8268c38de
Mathews <baiomathews@gmail.com>
2024-05-22 20:09:19 -0300

    método difference lodash

--- topico12_node03/js/script.js
+++ topico12_node03/js/script.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 let var01 = 10;
 
 const var02 = "gravidade";
@@ -9,3 +11,8 @@ function sub(a,b) {
 }
 
 console.log(sub(10,5));
+
+let array01 = ['banana', 'morango', 'maçã']
+let array02 = ['banana', 'morango', 'uva']
+let difference = _.difference(array01, array02);
+console.log(difference);
